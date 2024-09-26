8c3c65b15acfd0bc5a549fc40e0192f2f973198b
0xCastar <orshyemmanuel@gmail.com>
2024-09-26 23:19:17 +0100

    Setting up the first modal

--- app/page.js
+++ app/page.js
@@ -1,101 +1,152 @@
-import Image from "next/image";
+'use client'
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { address, abi } from "./constants.js";
+import './styles.css';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isConnected, setIsConnected] = useState(false);
+  const [signer, setSigner] = useState(null);
+  const [ethValue, setEthValue] = useState("");
+  const [contractBalance, setContractBalance] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  useEffect(() => {
+    const checkConnection = async () => {
+      if (typeof window.ethereum !== "undefined") {
+        const accounts = await window.ethereum.request({ method: "eth_accounts" });
+        if (accounts.length > 0) {
+          setIsConnected(true);
+          const provider = new ethers.BrowserProvider(window.ethereum);
+          const signer = await provider.getSigner();
+          setSigner(signer);
+          console.log("Wallet connected and signer set:", signer);
+        }
+      }
+    };
+
+    checkConnection();
+  }, []);
+
+  async function connectWallet() {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        setIsConnected(true);
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        setSigner(signer);
+        localStorage.setItem(isConnected, true);
+        console.log("Wallet connected and signer set:", signer);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      console.log("Please install MetaMask!");
+    }
+  }
+
+  const handleDisconnect = () => {
+    setIsConnected(false);
+    setSigner(null);
+    setEthValue("");
+    setContractBalance("");
+    localStorage.removeItem("isConnected");
+    console.log("Wallet disconnected.");
+  };
+
+
+  async function handleFund() {
+    if (isConnected && signer) {
+      const contract = new ethers.Contract(address, abi, signer);
+
+      try {
+        const transactionResponse = await contract.fund({
+          value: ethers.parseEther(ethValue),
+        });
+        await transactionResponse.wait(1);
+        console.log("Funding successful!");
+        console.log("The input value is:", ethValue);
+
+        setEthValue("");
+      } catch (error) {
+        console.log("Error during funding:", error);
+      }
+    } else {
+      console.log("Please connect your Metamask.");
+    }
+  }
+
+  async function handleWithdraw() {
+    if (isConnected && signer) {
+      const contract = new ethers.Contract(address, abi, signer);
+
+      try {
+        const balance = await contract.getBalance();
+        const transactionResponse = await contract.withdraw();
+        console.log("Withdraw Pending...");
+        await transactionResponse.wait(1);
+        console.log("Withdraw Sucessful!");
+
+      } catch (e) {
+        console.log("Withdrawal error")
+      }
+    } else {
+      console.log("Please connect your Metamask!")
+    }
+  }
+
+
+  async function handleGetBalance() {
+    if (isConnected && signer) {
+      const contract = new ethers.Contract(address, abi, signer);
+      try {
+        const balance = await contract.getBalance();
+        const formattedBalance = `${ethers.formatEther(balance)}eth`;
+        console.log(formattedBalance);
+        setContractBalance(formattedBalance)
+      } catch (e) {
+        console.log(e)
+      }
+    } else {
+      console.log("Please connect your Metamask!")
+    }
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      handleFund();
+    }
+  }
+
+  return (
+    <div>
+
+      <div className="connect-button-container">
+        {
+          isConnected ? <button className="connect-button" onClick={handleDisconnect}>Dissconect</button> :
+            <button className="connect-button" onClick={connectWallet}>Connect Wallet</button>
+        }
+      </div>
+
+      <div className="main-container">
+        <h1>CROWD FUNDING DAPP</h1>
+
+        <div className="page-container">
+          <input
+            type="number"
+            placeholder="Enter ETH amount"
+            value={ethValue}
+            onChange={(e) => setEthValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <button className="fund-button" onClick={handleFund}>Fund</button>
+
+          <button className="withdraw-button" onClick={handleWithdraw}>Withdraw</button>
+
+          <button className="balance-button" onClick={handleGetBalance}>Get Balance</button>
+          <div className="balance-div">Contract Balance: {contractBalance}</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
